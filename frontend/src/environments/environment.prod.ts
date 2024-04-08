99b440680af15aaae53023dfee2ba0e6f84b0f22
Monir11 <mounirrouissi2@gmail.com>
2024-04-08 11:09:37 +0200

    first ommit

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // shopApiUrl: 'https://angular-eshop-backend.herokuapp.com/api/v1',
+  shopApiUrl: 'http://localhost:8080/api/v1',
+  stripePublishableKey: "pk_test_51LeP5ZKJEbgQpy2wYSROThPfHg96irfZKvD05Acj42oMOwOJRdoIRtq8WldkYUkAQFRn8JRAWIxsg6JnT9vopcK100K6wQTWiv"
+}
